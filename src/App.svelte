b83844d6860c5dedfebaf41facf7871b3258c53c
Ole Umlauft <ole.umlauft@protonmail.com>
2022-07-26 00:20:18 +0200

    Add alternative image description

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <main class="grid">
   <div class="box">
-    <img src="image-qr-code.png" />
+    <img src="image-qr-code.png" alt="QR code" />
     <article>
       <h2>Improve your front-end skills by building projects</h2>
       <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
